e78e234351a9ac1ae232a6c38134856f3cfd656b
Hamidkev3 <46525162+Hamidkev3@users.noreply.github.com>
2020-12-02 14:21:00 +0330

    Update Python_best_ideas.py

--- Python_best_ideas.py
+++ Python_best_ideas.py
@@ -1,5 +1,3 @@
 def prime_numbers_lower_than_a_number(n):
     return(list(filter(lambda x: int(x) > 1 and (x == 2 or x == 3 or [x % i == 0 for i in range(2,int(x**0.5)+1)].count(True) == 0)
-    ,[i for i in range(int(input("Enter a number: ")))])))
-
-prime_numbers_lower_than_a_number(10)
+    ,[i for i in range(int(n))])))
